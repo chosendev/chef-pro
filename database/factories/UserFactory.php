515cb69d83a50fcaa5ad20f7c0a635a1c1a39c9b
NakasiDorothy <nakasidorothy0@gmail.com>
2019-05-09 11:33:31 +0300

    Table Seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,12 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\User;
+use App\staff;
+use App\StaffType;
+use App\item;
+use App\vendor;
+use App\store;
+use App\UsedItem;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -24,4 +30,63 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
+});
+
+$factory->define(StaffType::class, function(Faker $faker) {
+
+	return[
+		
+		'type' => $faker->randomElement(['Waiter','Cheff','Waitress','Cashier','Manager']),
+	];
+});
+
+$factory->define(staff::class, function(Faker $faker) {
+
+	return [
+		
+		'staff_type_id' => 1,
+		'first_name' => $faker->name,
+		'last_name' => $faker->name,
+		'phone_number' => $faker->randomNumber,
+		'address' => $faker->name,
+		'next_of_kin_name' => $faker->name,
+		'next_of_kin_phone_number' => $faker->randomNumber,
+	];
+});
+
+$factory->define(item::class, function(Faker $faker) {
+
+	return [
+		'name' => $faker->name,
+	];
+});
+
+$factory->define(vendor::class, function(Faker $faker) {
+
+	return [
+		'name' => $faker->name,
+		'phone_number' => $faker->randomNumber,
+		'item_id' => 1,
+		'location' => $faker->name,
+	];
+});
+
+$factory->define(store::class, function(Faker $faker) {
+
+	return [
+		'item_id' => 1,
+		'quantity_stocked' => $faker->randomNumber,
+		'status' => $faker->name,
+		'vendor_id'=>1,
+		'date' => date('Y-m-d'),
+	];
+});
+
+$factory->define(UsedItem::class, function(Faker $faker) {
+
+	return [
+		'quantity' => $faker->randomNumber,
+		'staff_id'=> 1,
+		'date' => date('Y-m-d'),
+	];
 });
